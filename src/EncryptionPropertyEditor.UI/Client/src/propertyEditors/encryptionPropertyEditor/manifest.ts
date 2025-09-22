1d82808c97e2089040f5be4dd90fe9febc798cee
Busra Sengul <aysebusrasengul@gmail.com>
2025-09-22 14:51:29 +0100

    Adding property editor settings

--- src/EncryptionPropertyEditor.UI/Client/src/propertyEditors/encryptionPropertyEditor/manifest.ts
+++ src/EncryptionPropertyEditor.UI/Client/src/propertyEditors/encryptionPropertyEditor/manifest.ts
@@ -9,7 +9,52 @@ const manifest: ManifestPropertyEditorUi = {
     label: "Encryption Property Editor",
     group: 'common',
     icon: "icon-list",
-    propertyEditorSchemaAlias: "Umbraco.Plain.String"
+    propertyEditorSchemaAlias: "Umb.PropertyEditorUi.TextBox",
+    settings: {
+      properties: [
+        {
+          alias: "defaultValue",
+          label: "Default Value",
+          description: "Provide a default value for the property",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.TextBox"
+        },
+        {
+          alias: "hideLabel",
+          label: "Hide Label?",
+          description: "Hide the property label.",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.Toggle"
+        },
+        {
+          alias: "key",
+          label: "Encryption Key?",
+          description: "Add your 64 char encryption key.",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.TextBox"
+        },
+        {
+          alias: "iv",
+          label: "Encryption IV?",
+          description: "Add your 32 char encryption IV.",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.TextBox"
+        },
+        {
+          alias: "password",
+          label: "Password?",
+          description: "Add your password.",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.TextBox"
+        }, {
+          alias: "salt",
+          label: "Salt?",
+          description: "Provide a random string of characters for hashing",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.TextBox"
+        }, {
+          alias: "useHash",
+          label: "Use Hash?",
+          description: "Use this only for password fields. (Irreversable Encryption).",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.Toggle"
+        },
+      ]
+    },
+
   },
 }
 
--- src/EncryptionPropertyEditor.UI/wwwroot/App_Plugins/EncryptionPropertyEditorUI/encryption-property-editor-ui.js
+++ src/EncryptionPropertyEditor.UI/wwwroot/App_Plugins/EncryptionPropertyEditorUI/encryption-property-editor-ui.js
@@ -1,11 +1,11 @@
-const t = [
+const r = [
   {
     name: "Encryption Property Editor UIEntrypoint",
     alias: "EncryptionPropertyEditor.UI.Entrypoint",
     type: "backofficeEntryPoint",
     js: () => import("./entrypoint-LW5tvUwM.js")
   }
-], o = {
+], i = {
   type: "propertyEditorUi",
   alias: "Umbraco.Community.EncryptionPropertyEditor",
   name: "Encryption Property Editor",
@@ -14,13 +14,59 @@ const t = [
     label: "Encryption Property Editor",
     group: "common",
     icon: "icon-list",
-    propertyEditorSchemaAlias: "Umbraco.Plain.String"
+    propertyEditorSchemaAlias: "Umbraco.Plain.String",
+    settings: {
+      properties: [
+        {
+          alias: "defaultValue",
+          label: "Default Value",
+          description: "Provide a default value for the property",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.TextBox"
+        },
+        {
+          alias: "hideLabel",
+          label: "Hide Label?",
+          description: "Hide the property label.",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.Toggle"
+        },
+        {
+          alias: "key",
+          label: "Encryption Key?",
+          description: "Add your 64 char encryption key.",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.TextBox"
+        },
+        {
+          alias: "iv",
+          label: "Encryption IV?",
+          description: "Add your 32 char encryption IV.",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.TextBox"
+        },
+        {
+          alias: "password",
+          label: "Password?",
+          description: "Add your password.",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.TextBox"
+        },
+        {
+          alias: "salt",
+          label: "Salt?",
+          description: "Provide a random string of characters for hashing",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.TextBox"
+        },
+        {
+          alias: "useHash",
+          label: "Use Hash?",
+          description: "Use this only for password fields. (Irreversable Encryption).",
+          propertyEditorUiAlias: "Umb.PropertyEditorUi.Toggle"
+        }
+      ]
+    }
   }
-}, r = [o], n = [
-  ...t,
-  ...r
+}, o = [i], t = [
+  ...r,
+  ...o
 ];
 export {
-  n as manifests
+  t as manifests
 };
 //# sourceMappingURL=encryption-property-editor-ui.js.map
